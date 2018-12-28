85e08f70e5e336e396a328a1f8d7f13f20133170
Alex Dehnert <adehnert@mit.edu>
2018-12-28 02:04:14 -0500

    mail merge: Link to mail merge info on GitLab Pages

--- Extras/gitlab-copy-docs.sh
+++ Extras/gitlab-copy-docs.sh
@@ -7,9 +7,15 @@ dest=$1
 cp $dest/index.html $dest/index-default.html
 cp Extras/gitlab-docs-index.html $dest/index.html
 cp artifacts/greensheets/guildcamp-git.pdf $dest/
-cp artifacts/greensheets/ci.pdf $dest/
 cp Extras/vgametex.py $dest/vgametex.py
+
+# CI
+cp artifacts/greensheets/ci.pdf $dest/
 cp Greensheets/Makefile $dest/Makefile-green
 cp Production/Makefile $dest/Makefile-prod
 grep -v mail_merge.py .circleci/config.yml > $dest/circleci-config.yml
 grep -v Extras/gitlab-copy-docs.sh .gitlab-ci.yml > $dest/gitlab-ci.yml
+
+# Mail merge
+cp Extras/mail_merge.py $dest/mail_merge.py
+cp Extras/README-mail-merge $dest/README-mail-merge.txt
