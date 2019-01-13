487a53b49f97adaac0ad32dd323e260dbb890ec0
Alex Dehnert <adehnert@mit.edu>
2019-01-13 14:48:59 -0500

    install: Add actual install mechanism

--- Extras/install.py
+++ Extras/install.py
@@ -17,25 +17,50 @@ import argparse
 import os.path
 import shutil
 import subprocess
+import sys
+import tarfile
+import tempfile
 
 import logging
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)  # pylint: disable=invalid-name
 
 
-class F:  # pylint: disable=invalid-name,too-few-public-methods
+class F:  # pylint: disable=invalid-name
     """
     Represents a file to distribute
     """
 
-    def __init__(self, repo, site="", exclude=None, git_rel=False):
+    def __init__(self, repo, site="",  # pylint: disable=too-many-arguments
+                 exclude=None, install=True, git_rel=False):
         """
         Initialize object
         """
         self.repo = repo
         self.site = site or os.path.basename(repo)
         self.exclude = exclude
+        self.install = install
         self.git_rel = git_rel
+
+    def fixup_tar_install(self, git_parent):
+        """
+        Move any installed file that is installed to the wrong place by tar
+        """
+        if self.git_rel and git_parent:
+            # We want to move the whole subtree (eg, .circleci), not just the
+            # single file (.circleci/config.yml), to be a direct sibling of
+            # .git.  If we ever get multiple files in a git-relative directory,
+            # or become concerned one of the directories might already exist
+            # and have content we shouldn't move, we'll need to fix this.
+            dir_name = self.repo.split('/')[0]
+            shutil.move(dir_name, '..')
+
+    def add_to_tar_install(self, tar):
+        """
+        Add file to installer tarfile
+        """
+        if self.install:
+            tar.add(self.repo)
 
     def copy_to_site(self, site_dir):
         """
@@ -53,21 +78,26 @@ class F:  # pylint: disable=invalid-name,too-few-public-methods
 
 
 FILES = [
-    F("Extras/gitlab-docs-index.html", "index.html"),
-    F("artifacts/greensheets/guildcamp-git.pdf"),
+    F("Extras/gitlab-docs-index.html", "index.html", install=False),
+    F("artifacts/greensheets/guildcamp-git.pdf", install=False),
     F("Extras/vgametex.py"),
+    F("Extras/install.py"),
 
     # CI
-    F("artifacts/greensheets/ci.pdf"),
+    F("artifacts/greensheets/ci.pdf", install=False),
     F("Greensheets/Makefile", "Makefile-green"),
     F("Production/Makefile", "Makefile-prod"),
-    F(".circleci/config.yml", "circleci-config.yml", exclude="mail_merge.py"),
-    F(".gitlab-ci.yml", "gitlab-ci.yml", exclude="copy_to_site"),
+    F(".circleci/config.yml", "circleci-config.yml", exclude="mail_merge.py",
+      git_rel=True),
+    F(".gitlab-ci.yml", "gitlab-ci.yml", exclude="copy_to_site",
+      git_rel=True),
 
     # Mail merge
     F("Extras/mail_merge.py"),
     F("Extras/README-mail-merge"),
 ]
+
+TARFILE_NAME = 'addons.tar'
 
 
 def parse_args():
@@ -84,8 +114,59 @@ def parse_args():
     copy_to_site_parser = subparsers.add_parser("copy_to_site")
     copy_to_site_parser.add_argument("dest")
 
-    args = parser.parse_args()
+    # argparse doesn't seem to support a default subparser. This hack will
+    # use install if none is specified, with the default values.
+    argv = sys.argv[1:]
+    if not argv:
+        argv += ['install']
+
+    args = parser.parse_args(argv)
     return args
+
+
+def fetch(url, dest):
+    """
+    Fetch a file from <url> to <dest>
+    """
+    log.info("Downloading %s to %s", url, dest)
+    subprocess.check_call(["curl", "-o", dest, url])
+
+
+def install_from_tar(files, webbase):
+    """
+    Download and install addons using tar file
+    """
+    if not os.path.isdir('LaTeX'):
+        sys.exit("Installer must be called from the top of your GameTeX "
+                 "install (the directory containing LaTeX/, Production/, and "
+                 "so forth")
+
+    prefix = 'gametex-addons-'
+    with tempfile.NamedTemporaryFile(prefix=prefix, suffix='.tar') as tar:
+        fetch(webbase+'/'+TARFILE_NAME, tar.name)
+        subprocess.check_call(['tar', '-xvf', tar.name])
+
+    if os.path.isdir('.git'):
+        git_parent = False
+    elif os.path.isdir('../.git'):
+        git_parent = True
+    else:
+        log.warning("Neither current nor parent directory contains a .git "
+                    "directory. Not bothering to move .git-relative files, "
+                    "such as CI configuration.")
+        git_parent = None
+
+    for file_obj in files:
+        file_obj.fixup_tar_install(git_parent=git_parent)
+
+
+def create_tar(files, tarname):
+    """
+    Create tarfile with the addon files
+    """
+    with tarfile.open(tarname, mode='w') as tar:
+        for file_obj in files:
+            file_obj.add_to_tar_install(tar)
 
 
 def copy_to_site(files, site_dir):
@@ -97,6 +178,7 @@ def copy_to_site(files, site_dir):
 
     for file_obj in files:
         file_obj.copy_to_site(site_dir)
+    create_tar(files, os.path.join(site_dir, TARFILE_NAME))
 
 
 def main():
@@ -106,7 +188,7 @@ def main():
     args = parse_args()
 
     if args.mode == 'install':
-        raise ValueError("not implemented")
+        install_from_tar(FILES, args.webbase)
     elif args.mode == 'copy_to_site':
         copy_to_site(FILES, args.dest)
     else:
